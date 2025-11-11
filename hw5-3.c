901daee3ccbdbb89c5da22bb7086dd6dc98737b2
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-11-12 00:24:28 +0900

    Add input validation for different numbers

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main (void) {
+  int x, y;
+  printf("Please enter two different numbers\n");
+  scanf("%d", &x);
+  scanf("%d", &y);
+  while (x==y) {
+    printf("Same, please enter two different numbers\n");
+    scanf("%d", &x);
+    scanf("%d", &y);
+  }
+  printf("You entered %d and %d", x, y);
+  return 0;
+}
