fe37f5e2a4b439a5f725ffc60eacbbf7085f614e
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-11-12 00:23:51 +0900

    Implement powers of 2 printing up to user input
    Add a program that prints powers of 2 up to max.

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main (void){
+  int max;
+  int i = 1;
+  printf("max?\n");
+  scanf("%d", &max);
+  while (i<=max) {
+    printf ("%d ", i);
+    i *= 2;
+  }
+  return 0;
+}
