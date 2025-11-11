c5e63220881bc25d5f8215422143932b3ce8df22
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-11-12 00:22:07 +0900

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,13 @@
-
+5-1:
+#include <stdio.h>
+int main (void){
+  int max;
+  int i = 1;
+  printf("max?\n");
+  scanf("%d", &max);
+  while (i<=max) {
+    printf ("%d ", i);
+    i *= 2;
+  }
+  return 0;
+}
