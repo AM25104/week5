092646df38791535ad593cdb1fef9013a1023905
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-11-12 00:22:43 +0900

    Implement divisor printing program in hw5-2.c
    Add a program to print all divisors of a number

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+int main (void) {
+  int max;
+  int i = 1;
+  printf("max?\n");
+  scanf ("%d", &max);
+  while (i <= max){
+    if (max % i == 0) {
+      printf("%d ", i);
+    }
+    i++;
+  }
+  return 0;
+}
