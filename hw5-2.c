b350a5dd69a095542cbed7734c2c29228851a8c2
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2026-01-30 12:07:11 +0900

    Add divisor printing functionality
    Implement a program to print all divisors of a given number.

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int max;
+    int first = 1;
+
+    printf("max?\n");
+    if (scanf("%d", &max) != 1) {
+        return 0;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        if (max % i == 0) {
+            if (!first) {
+                printf(" ");
+            }
+            printf("%d", i);
+            first = 0;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
 
